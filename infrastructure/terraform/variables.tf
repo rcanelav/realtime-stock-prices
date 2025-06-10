9e432dc74d6d1202fda4a77127758216e87cd74a
Ramón Canela Villamizar <rcanelav@gmail.com>
2025-06-10 23:08:20 +0200

    ADD: tf global vars

---
+++ infrastructure/terraform/variables.tf
@@ -0,0 +1,24 @@
+# ################################################################################
+#  This file describes the variables that are used in the app
+# ################################################################################
+variable "AWS_BEDROCK_MODEL_ID" {
+  description = "The model ID for the Bedrock model"
+  type        = string
+}
+
+variable "AWS_REGION" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+}
+
+variable "APP_NAME" {
+  description = "The name of the application"
+  type        = string
+  default     = "finance-agent"
+}
+
+variable "CONTAINER_PORT" {
+  description = "The port the container listens on"
+  type        = number
+  default     = 8000
+}
